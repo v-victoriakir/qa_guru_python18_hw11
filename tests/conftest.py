cfe9e40ccbb5831a4b89cd1748a37bbb4c7574df
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-03-10 16:57:31 +0300

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,16 +9,13 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
-
 @pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
 
-
 selenoid_login = os.getenv("SELENOID_LOGIN")
 selenoid_pass = os.getenv("SELENOID_PASS")
 selenoid_url = os.getenv("SELENOID_URL")
-
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_config(request):
@@ -34,7 +31,7 @@ def browser_config(request):
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}//wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options)
 
     browser.config.driver = driver
