5368c19d3c54f6be990ba9abab3b2b2984e76cd6
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-03-10 12:44:37 +0300

    initial preparation

---
+++ tests/conftest.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_config():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = "eager"
+    browser.config.driver_options = driver_options
+
+    browser.config.window_height = 2500
+    browser.config.window_width = 1400
+
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.add_argument("--headless")
+    # browser.config.driver_options = driver_options
+
+    browser.config.type_by_js = True
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope="function")
+def today_date():
+    c = datetime.now()
+    current_time = c.strftime("%d %B, %Y")
