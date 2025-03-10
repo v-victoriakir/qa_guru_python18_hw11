81c099234cd5b8632f1eda941141db951d7f98d6
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-03-10 17:19:40 +0300

    исправила login / pass

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def browser_config(request):
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user_k:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
