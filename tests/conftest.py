01ae4e3167de976a24eb8da8a6b72d45c04ea603
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-03-10 17:13:40 +0300

    убрала env для сравнения

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,14 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
-
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
+# @pytest.fixture(scope="session", autouse=True)
+# def load_env():
+#     load_dotenv()
+#     selenoid_login = os.getenv("SELENOID_LOGIN")
+#     selenoid_pass = os.getenv("SELENOID_PASS")
+#     selenoid_url = os.getenv("SELENOID_URL")
+#
+#     print(selenoid_login)
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_config(request):
@@ -31,7 +32,7 @@ def browser_config(request):
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f"https://user_k:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
